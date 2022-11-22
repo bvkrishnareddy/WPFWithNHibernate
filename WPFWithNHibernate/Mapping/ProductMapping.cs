a37c6a999655b2fa680e42a1d0b6b2541fc32f73
bvkrishnareddy <79552444+bvkrishnareddy@users.noreply.github.com>
2022-11-22 12:39:21 +0530

    Add files via upload

---
+++ WPFWithNHibernate/Mapping/ProductMapping.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFWithNHibernate.Model;
+
+namespace WPFWithNHibernate.Mapping
+{
+    public class ProductMapping : ClassMap<Product>
+    {
+        public ProductMapping()
+        {
+            Id(x => x.ID);
+            Map(x => x.Name).Not.Nullable();
+            Map(x => x.Category).Not.Nullable();
+            Table("Product");
+        }
+    }
+}
---
+++ WPFWithNHibernate/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFWithNHibernate.Model
+{
+    public class Product
+    {
+        public virtual int ID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Category { get; set; }
+    }
+}
